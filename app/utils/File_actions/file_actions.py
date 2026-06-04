05b77d562c9020cbea6eedf1eabba3fc2fa4ee43
Feuder <caspargr@outlook.de>
2026-06-05 01:55:30 +0200

    kleine änderungen mit keine Ahung was für dateien

--- app/utils/File_actions/file_actions.py
+++ app/utils/File_actions/file_actions.py
@@ -8,12 +8,15 @@ def move_file(datei_name, alter_pfad, neuer_pfad):
         shutil.move(alter_pfad, neuer_pfad)
         
         logging.info(
+        "-" * 50 + "\n" +
         f"Die Datei {datei_name} wurde erfolgreich verschoben:\n" \
         f"Alter Speicherort: {alter_pfad}\n" \
         f"Neuer Pfad: {neuer_pfad}"
+        "-" * 50
         )
     except Exception as f:
         print("Es gab einen Fehler bei dem versuch folgende Datei zu verschieben:\n" \
+        "-" * 50 + "\n" \
         f"Dateiname: {datei_name}")
         f"Fehlermeldung: {f}"
 
@@ -31,6 +34,7 @@ def rename_file(datei_name, neuer_name, alter_pfad):
 
     except Exception as f:
         print("Es gab einen Fehler bei dem versuch folgende Datei zu verschieben:\n" \
+        "-" * 50 + "\n " \
         f"Dateiname: {datei_name}")
         f"Fehlermeldung: {f}"
         
